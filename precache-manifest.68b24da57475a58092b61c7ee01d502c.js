76180d3826a21d042855d34e3c4c463dbf9bfb84
Mike Guggenbuehl <guggenz@Mikes-MacBook-Pro.local>
2020-09-14 21:21:05 -0500

    Updates

---
+++ precache-manifest.68b24da57475a58092b61c7ee01d502c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee6efde955dad0c1ecfa07e4a707a3ea",
+    "url": "/chronicle-wellness/index.html"
+  },
+  {
+    "revision": "aed56d1a12175ab7016c",
+    "url": "/chronicle-wellness/static/css/main.4e9f4f5c.chunk.css"
+  },
+  {
+    "revision": "991b1b70bb1ff34de9a2",
+    "url": "/chronicle-wellness/static/js/2.9af99c57.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/chronicle-wellness/static/js/2.9af99c57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aed56d1a12175ab7016c",
+    "url": "/chronicle-wellness/static/js/main.a9885626.chunk.js"
+  },
+  {
+    "revision": "91c95aaa3def90efa63c",
+    "url": "/chronicle-wellness/static/js/runtime-main.ddcb8a6a.js"
+  }
+]);
